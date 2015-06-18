cb389e83fd28520901b7bff05bc41ed1abcf9469
Alex Weber <alexweber15@gmail.com>
2015-06-17 21:08:44 -0300

    attempting to create table automatically

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import firenze from 'firenze/lib';
 import db from './database';
 import Post from './model';
 import Posts from './collection';
+
+let postModel = new Post();
+
+db.getAdapter().createTable(postModel);
